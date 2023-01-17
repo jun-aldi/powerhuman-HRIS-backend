0cf8543fa2cc93f680208a00f6df797e1506e828
juniaraldi <aldinugraha708@gmail.com>
2023-01-17 23:24:45 +0700

    faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Company;
+use App\Models\Team;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(2)->unverified()->create();
+        // \App\Models\Company::factory(10)->create();
+        // Company::factory(10)->create();
+        // Team::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
