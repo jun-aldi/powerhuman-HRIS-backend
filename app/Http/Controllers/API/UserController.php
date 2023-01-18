5115e816e58159f573f50b3e456e71e7f6f2354a
juniaraldi <aldinugraha708@gmail.com>
2023-01-18 16:13:03 +0700

    User Auth Controller

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,10 +2,109 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Helpers\ResponseFormatter;
+use Exception;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Fortify\Rules\Password;
+use PhpParser\Node\Stmt\TryCatch;
 
 class UserController extends Controller
 {
-    //
+    //login
+    public function login(Request $request)
+    {
+        try {
+            // Validate Request
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            //Find user by email
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error('Unauthorized', 401);
+            }
+
+            $user = User::where('email', $request->email)->first(); //intinya nampilin data user yang emailnya sudah kita validasi
+
+            //ngecek password udah sesuai apa belom
+            if (!Hash::check($request->password, $user->password)) {
+                throw new Exception('Invalid Password');
+            }
+
+            //Generate Token
+            //kita pakai dari sanctum create token
+            $toketResult = $user->createToken('authToken')->plainTextToken;
+
+            //Return Response
+            //ngembaliiin data response token
+            return ResponseFormatter::success([
+                'access_token' => $toketResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Login Success');
+        } catch (Exception $error) {
+            return ResponseFormatter::error('Authentication Failed');
+        }
+    }
+
+    //register
+    public function register(Request $request)
+    {
+        try {
+            //Validate Request
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', new Password]
+            ]);
+
+            //Create User
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            //Generate Token
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            //Return Response
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Register Success');
+        } catch (Exception $error) {
+            //Retrun error Response
+            return ResponseFormatter::error($error->getMessage());
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        // Revoke Token
+        $token = $request->user()->currentAccessToken()->delete();
+
+        // Return Response
+        return ResponseFormatter::success($token, 'Logout success');
+    }
+
+
+    // Ngambilin data user
+    //cuman ngambil data user
+    // harus login dulu
+    public function fetch(Request $request)
+    {
+        // Get user
+        $user = $request->user();
+
+        // Return Response
+        return ResponseFormatter::success($user, 'Fetch Success');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\CompanyController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('/company', [CompanyController::class, 'all']);
+
+
+Route::post('login', [UserController::class, 'login'])->name('login');
+Route::post('register', [UserController::class, 'register'])->name('register');
+//logout karena ambil token login pakai middleware
+Route::post('logout', [UserController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
+//fetch harus login dl
+Route::get('user', [UserController::class, 'fetch'])->name('user')->middleware('auth:sanctum');
